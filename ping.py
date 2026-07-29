141fde854a2d2eabad926fd1ad2fee31c9c3c51b
asuwanphan-gif <a.suwanphan@sskru.ac.th>
2026-07-29 10:42:58 +0700

    Add aphanssk-pixel project + fix ping status reporting (404/paused no longer shown as OK)

--- ping.py
+++ ping.py
@@ -30,6 +30,11 @@ PROJECTS = [
    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
    ".eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ2eWhwZ3ZtZHB6a2lodnJhdGJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk2MTY1MzEsImV4cCI6MjA5NTE5MjUzMX0"
    ".P7djsZcyLxjeCLAGAgKfBQLO4kJ6BlsrqXtn1FUJ5xw"),
+  ("aphanssk-pixel",
+   "https://wdosfpfpslghckedytju.supabase.co",
+   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9"
+   ".eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indkb3NmcGZwc2xnaGNrZWR5dGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MTA4NjEsImV4cCI6MjEwMDI4Njg2MX0"
+   ".YdFVMgxIlnrWLexSb6z_KRtpImm481IYIuSA-u0VpWc"),
 ]
 for name, url, key in PROJECTS:
     try:
@@ -46,9 +51,11 @@ for name, url, key in PROJECTS:
         with urllib.request.urlopen(req, timeout=15) as r:
             print("[OK]   " + name + " -> HTTP " + str(r.status))
     except urllib.error.HTTPError as e:
-        if 200 <= e.code < 500:
+        if 200 <= e.code < 300:
             print("[OK]   " + name + " -> HTTP " + str(e.code))
+        elif e.code == 404:
+            print("[FAIL] " + name + " -> HTTP 404 (ping() function missing)")
         else:
-            print("[FAIL] " + name + " -> HTTP " + str(e.code) + " (paused)")
+            print("[FAIL] " + name + " -> HTTP " + str(e.code) + " (paused/blocked)")
     except Exception as e:
         print("[FAIL] " + name + " -> ERROR: " + str(e))
